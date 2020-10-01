51abe09838984153bbea6fe879ae8173e62973ab
naganthanganeshbabu <naganathanganeshbabu@syncfusion.com>
2020-10-01 11:57:01 +0530

    Mouse move event with some interval values sample using Lodash throttle committed.

---
+++ interop.js
@@ -0,0 +1,18 @@
+﻿
+
+
+function onMouseDown(elem, component, interval) {
+    elem.addEventListener('mousedown', _.throttle(e => {
+        component.invokeMethodAsync('HandleMouseDown', e.offsetX, e.offsetY);
+    }, interval));
+};
+
+function onMouseUp(elem, component, interval) {
+    elem.addEventListener('mousemove', kk);
+};
+
+function onMouseMoveOninit(elem, component, interval) {
+    elem.addEventListener('mousemove', _.throttle(e => {
+        component.invokeMethodAsync('HandleMouseMove', e.clientX, e.clientY);
+    }, interval));
+};
